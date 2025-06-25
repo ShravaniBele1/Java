1c8d47c62595350604d446d493fe8c5340461379
ShravaniBele1 <shravanibele1008@gmail.com>
2025-06-25 11:42:49 +0530

    Add files via upload

---
+++ MaximumNumber.java
@@ -0,0 +1,23 @@
+
+public class MaximumNumber {
+
+	public static void main(String[] args) {
+		int a=8;
+		int b=5;
+		int c=3;
+		
+		if(a>b && a>c)
+		{
+			System.out.println(a+" is a max number");
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println(b+" is a max number");
+		}
+		else
+		{
+			System.out.println(c+" is a max number");
+		}
+	}
+
+}
